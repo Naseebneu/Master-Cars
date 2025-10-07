fdce2159c8b9e5a44e338972cc03d0581ef94a33
Naseebneu <naseebmkt1997@outlook.com>
2025-10-07 10:48:12 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
 const API_URL = "https://your-site.netlify.app/.netlify/functions/carquery";
+return {
+  statusCode: 200,
+  body: JSON.stringify(data),
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
+  }
+};
+
